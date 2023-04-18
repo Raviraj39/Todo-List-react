802449977b6973403271f189b6a875c86b556e97
Raviraj39 <rravirajbujawade@gmail.com>
2023-04-18 17:50:13 +0530

    Added All File And packages

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Form(props) {
+  const [name, setName] = useState("");
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.addTask(name);
+    setName("");
+  }
+  const notify = () => {
+toast.success('Task Added !', {
+            position: toast.POSITION.TOP_RIGHT
+        });
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
-        Today's List
+          What needs to be done?
         </label>
       </h2>
       <input
@@ -14,11 +33,16 @@ function Form(props) {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
-      <button type="submit" className="btn btn__primary btn__lg">
+      <button type="submit" onClick={notify} className="btn btn__primary btn__lg">
         Add
       </button>
+              <ToastContainer />
+
     </form>
+    
   );
 }
 
